e2dd0f82543d7c3170a010c2234eeb1da3021d3e
Revathi AR <revathi07saran@gmail.com>
2021-02-07 20:24:47 +0530

    Project created

---
+++ src/Task_0702/Task.java
@@ -0,0 +1,18 @@
+package Task_0702;
+
+public class Task {
+
+	public static void main(String[] args) {
+		
+		int Mark1 = 40;
+		//int Mark2 =20;
+		if (Mark1 >= 40) {
+System.out.println("Pass");
+	}else {
+		System.out.println("Fail");
+	}
+
+
+	}
+
+}
